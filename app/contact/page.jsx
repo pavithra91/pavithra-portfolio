043b4294bf69e633b15d8b216e707b90de2f4e55
pavithra91 <dev.pavithraj@gmail.com>
2024-07-16 12:52:10 +0530

    commit

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,103 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@radix-ui/react-select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+94) 715 918 243",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "pavi.dsscst@gmail.com",
+  },
+  {
+    icon: <FaMapMarkedAlt />,
+    title: "Address",
+    description: "No: 65/A, Bathiya Mawatha Pamunuwilla, Gonawala (WP)",
+  },
+];
+
+import { motion } from "framer-motion";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:w-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent">Let's Work together</h3>
+              <p className="text-white/60">
+                I'm a Passionate software developer who&apos;s seeking new
+                opportunities. Let's talk!
+              </p>
+              {/* input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="First name" />
+                <Input type="lastname" placeholder="Last name" />
+                <Input type="email" placeholder="Email address" />
+                <Input type="phone" placeholder="Phone" />
+
+                {/* textarea */}
+                <Textarea
+                  className="h-[200px]"
+                  placeholder="Type your message here"
+                />
+
+                {/* button */}
+                <Button size="md" className="max-w-40">
+                  Send Message
+                </Button>
+              </div>
+            </form>
+          </div>
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul>
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contact;
--- app/work/page.jsx
+++ app/work/page.jsx
@@ -17,6 +17,7 @@ import {
 
 import Link from "next/link";
 import Image from "next/image";
+import WorkSliderBtns from "@/components/ui/WorkSliderBtns";
 
 const projects = [
   {
@@ -36,6 +37,30 @@ const projects = [
       },
     ],
     image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "Full-stack",
+    title: "Payment Gateway integration",
+    description: "",
+    stack: [
+      {
+        name: ".Net",
+      },
+    ],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "Full-stack",
+    title: "SAP Company Configuration",
+    description: "",
+    stack: [],
+    image: "/assets/work/thumb3.png",
     live: "",
     github: "",
   },
@@ -63,13 +88,13 @@ const Work = () => {
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
             <div className="flex flex-col gap-[30px] h-[50%]">
               {/* outline num */}
-              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+              <div className="text-8xl leading-none font-extrabold text-outline">
                 {project.num}
               </div>
 
               {/* project Category */}
               <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
-                {project.category} project
+                {project.title}
               </h2>
               {/* project description */}
               <p className="text-white/60">{project.description}</p>
@@ -142,6 +167,12 @@ const Work = () => {
                   </SwiperSlide>
                 );
               })}
+
+              {/* slider button */}
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+              />
             </Swiper>
           </div>
         </div>
